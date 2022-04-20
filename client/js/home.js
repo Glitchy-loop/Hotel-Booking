822ac7e7360a2108c52339ef1aad1efe431b3a58
Audrius <darkinforest@gmail.com>
2022-04-20 21:24:39 +0300

    dates, js

--- client/js/home.js
+++ client/js/home.js
@@ -5,3 +5,35 @@ for (var i = 0; i < document.links.length; i++) {
   }
 }
 document.links[current].className = 'current'
+
+const startDate = document.querySelector('#checkIn')
+
+startDate.addEventListener('change', e => {
+  const startDateValue = startDate.value
+
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Dec']
+  const day = new Date(startDateValue).getDate()
+  const month = new Date(startDateValue).getMonth()
+
+  let checkInMonth = document.querySelector('#checkInas .month')
+  checkInMonth.innerHTML = months[month]
+
+  let checkInDay = document.querySelector('#checkInas .day')
+  checkInDay.innerHTML = day
+})
+
+const finishDate = document.querySelector('#checkOut')
+
+finishDate.addEventListener('change', e => {
+  const startDateValue = finishDate.value
+
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Dec']
+  const day = new Date(startDateValue).getDate()
+  const month = new Date(startDateValue).getMonth()
+
+  let checkInMonth = document.querySelector('#checkOutas .month')
+  checkInMonth.innerHTML = months[month]
+
+  let checkInDay = document.querySelector('#checkOutas .day')
+  checkInDay.innerHTML = day
+})
