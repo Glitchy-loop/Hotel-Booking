de2da451c4e936a9ee21ee48f87eaeec8a5ac6b2
Audrius <darkinforest@gmail.com>
2022-04-21 22:15:38 +0300

    added full details get fetch

--- client/js/rooms.js
+++ client/js/rooms.js
@@ -21,6 +21,8 @@ const getRooms = async () => {
     div.className = 'room'
 
     const img = document.createElement('img')
+    img.src =
+      'https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221_960_720.jpg'
 
     const title = document.createElement('div')
     title.className = 'title'
@@ -28,15 +30,27 @@ const getRooms = async () => {
 
     const guests = document.createElement('div')
     guests.className = 'guests'
-    guests.textContent = room.room_capacity
+
+    const guestsCount = document.createElement('div')
+    guestsCount.className = 'guestsCount'
+    guestsCount.textContent = `${room.room_capacity} Guests`
+
+    const rating = document.createElement('div')
+    rating.className = 'rating'
+    rating.textContent = `${room.hotel_rating} /5`
 
     const desc = document.createElement('div')
     desc.className = 'description'
     desc.textContent = room.room_description
 
+    const bookNowBtn = document.createElement('button')
+    bookNowBtn.className = 'bookNow'
+    bookNowBtn.textContent = `BOOK NOW FOR ${room.room_price}$`
+
+    guests.append(guestsCount, rating)
     rooms.append(div)
 
-    div.append(img, title, guests, desc)
+    div.append(img, title, guests, desc, bookNowBtn)
   })
 }
 
